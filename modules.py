c8dd9374903515d7f2c60b3643bde670dcbd2d2c
bezuidenhoutMC <tiaanbez@e-10lpac0utget.ds.man.ac.uk>
2024-12-03 10:55:22 +0200

    adding modules.py and pyproject.toml

---
+++ modules.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+def apps():
+    return
+
+def utils():
+    return
+
+def tests():
+    print("tests")
+
+
+if __name__ == '__main__':
+    apps
+    utils
+    tests
+    print("oooo")
